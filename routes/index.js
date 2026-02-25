9d83423f9ec13a050a8387130356c9b6c4d97b43
Matthew <smearygeary613@gmail.com>
2026-02-25 13:01:34 -0700

    fixes to indx.js

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const {
   validateLogin,
   validateCreateUser,
 } = require("../middlewares/validation");
+const clothingitems = require("./clothingitems");
 
 router.post("/signin", validateLogin, login);
 router.post("/signup", validateCreateUser, createUser);
 
 router.use("/users", userRouter);
+router.use("/items", clothingitems);
 
 router.use((req, res, next) => {
   next(new NotFoundError("Requested resource not found"));
